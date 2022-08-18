491c16e0f5576c278837711e95adde8614702d00
John Adebayo <adejoy28@gmail.com>
2022-08-18 12:27:10 -0700

    secon commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var options = {
+  strings: ['John Adebayo', 'a Web Developer', 'a UI Designer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true
+};
+
+var typed = new Typed('.typing-def', options);
